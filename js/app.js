281a94553dfea4609d3be781ad114b2306e76300
prokash Pul <pprokash102@gmail.com>
2022-02-26 16:30:08 +0600

    data connet and display apply

--- js/app.js
+++ js/app.js
@@ -30,10 +30,55 @@ function days() {
 // dispaly data 
 const dataDispaly = (data) => {
     console.log(data.response)
+        // cases tody update loop
     let newcas = 0;
-    for (const i of data.response) {
-        console.log(i)
-        newcas = i.cases.new;
+    let active = 0;
+    let recovered = 0;
+    let totalCases = 0;
+    let newDeath = 0;
+    let totalDeath = 0;
+    let newCriticals = 0;
+    for (const covidData of data.response) {
+        console.log(covidData)
+            // new cases
+        newcas = covidData.cases.new;
+        // active now
+        active = covidData.cases.active;
+        // active now
+        recovered = covidData.cases.recovered;
+        // total Cases
+        totalCases = covidData.cases.total;
+        // new deaths
+        newDeath = covidData.deaths.new;
+        // new deaths
+        totalDeath = covidData.deaths.total;
+        // new deaths
+        newCriticals = covidData.cases.critical;
+
+
     }
+
     console.log("new" + newcas)
+        // dom event handaling
+        // new new-cases
+    const newCases = document.getElementById('new-cases')
+    newCases.innerText = newcas;
+    // new Total active
+    const newActive = document.getElementById('active')
+    newActive.innerText = active;
+    // new Total active
+    const totalRecovered = document.getElementById('recovered')
+    totalRecovered.innerText = recovered;
+    // new New critical 
+    const newCritical = document.getElementById('critical')
+    newCritical.innerText = newCriticals;
+    // new Total active
+    const TotalCas = document.getElementById('total-cases')
+    TotalCas.innerText = totalCases;
+    // new new Deaths
+    const newDeaths = document.getElementById('new-deaths')
+    newDeaths.innerText = newDeath;
+    // new new Deaths
+    const totalDeaths = document.getElementById('total-death')
+    totalDeaths.innerText = totalDeath;
 }
